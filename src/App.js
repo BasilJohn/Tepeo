15f39c001800b508e97f5e7b696e76667b879b9b
Basil John <basiljohn@Basils-MacBook-Pro.local>
2023-02-03 14:51:29 +0000

    Basic Setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
+import imageOne from './img/imageOne.png';
+import imageTwo from './img/imageTwo.png'
+import imageThree from './img/imageThree.png'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='section-wrapper'>
+      <section className='section-container'>
+        <div className='section-left'>
+          <img alt='imageOne' className='image-style' src={imageOne} />
+        </div>
+        <div className='section-right'>
+          <div className='text-container'>
+            <p className='text-title'>
+              Heating your home,<br /><span>not the planet</span>
+            </p>
+            <p className='text-paragraph'>
+              The future of green sustainable heating is already here
+            </p>
+          </div>
+        </div>
+      </section>
+      <section className='section-container'>
+        <div className='section-left'>
+          <div className='text-container'>
+            <p className='text-title'>
+              The future of heating is<br />
+              <span>clean</span>,&nbsp;
+              <span>green</span>
+              &nbsp;and&nbsp;
+              <span>affordable</span>
+            </p>
+            <p className='text-paragraph'>
+              Our patent pending Zero Emission Boiler (ZEB) technology is revolutionising the way we heat our homes
+            </p>
+          </div>
+        </div>
+        <div className='section-right'>
+          <img alt='imageTwo' className='image-style' src={imageTwo} />
+        </div>
+      </section>
+      <section className='section-container'>
+        <div className='section-left'>
+          <img alt='imageOne' className='image-style' src={imageThree} />
+        </div>
+        <div className='section-right'>
+          <div className='text-container'>
+            <p className='text-title'>
+              Use the <span>power you generate</span>
+            </p>
+            <p className='text-paragraph'>
+              If you have rooftop solar PV a ZEB can automatically store excess electricity produced so you don’t unnecessarily export it to the grid
+            </p>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
